98e2ba3129047d626e44818383f704837b446e7a
serhatprogramming <serhatprogramming@gmail.com>
2023-06-14 15:38:00 -0400

    api url is updated.

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3001/api/movies";
+const API_URL = "/api/movies";
 
 const getAll = async () => {
   const response = await axios.get(API_URL);
